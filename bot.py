e8491041e77978fce45b0d3a15068fa96c98daf9
KimH4nKyul <alskim93@kakao.com>
2021-03-29 05:27:47 +0900

    .

---
+++ bot.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import discord
+import requests
+import asyncio
+import time
+import os
+from json import loads
+
+twitch_Client_ID = 'i1fqy0yyibvumjfogef17mqfuebsja'
+twitch_Client_secret = '33836pkseoqqqx2wem7a933lzckbm4'
+discord_Token = os.environ['token']
+discord_channelID = 825793305983582298
+# discord_channelID = 505258184634531847
+discord_bot_state = ''
+twitchID = 'tattoob0y'
+# twitchID = 'screamdaddy93'
+msg = ''
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+
+    print(client.user.id)
+    print("ready")
+
+    # 디스코드 봇 상태 설정
+    game = discord.Game(discord_bot_state)
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+    # 채팅 채널 설정
+    channel = client.get_channel(discord_channelID)
+    await client.wait_until_ready()
+    print(channel)
+
+    # 트위치 api 2차인증
+    oauth_key = requests.post("https://id.twitch.tv/oauth2/token?client_id=" + twitch_Client_ID +
+                              "&client_secret=" + twitch_Client_secret + "&grant_type=client_credentials")
+    access_token = loads(oauth_key.text)["access_token"]
+    token_type = 'Bearer '
+    authorization = token_type + access_token
+    print(authorization)
+
+    check = False
+
+    while client.is_ready:
+        print("ready on Notification")
+        headers = {'client-id': twitch_Client_ID,
+                   'Authorization': authorization}
+
+        response_channel = requests.get(
+            'https://api.twitch.tv/helix/streams?user_login=' + twitchID, headers=headers)
+        print("server: ", response_channel.text)
+
+        try:
+            if loads(response_channel.text)['data'][0]['type'] == 'live' and check == False:
+                # await client.wait_until_ready()
+                msg = time.strftime('%Y%m%d', time.localtime(
+                    time.time())) + ' 타뱅온! 방송보러가기 : https://www.twitch.tv/' + twitchID
+                await channel.send(msg)
+                print("Online")
+                check = True
+        except:
+            check = False
+            # msg = '현재는 방송 중이 아닙니다! '
+            # await channel.send(msg)
+            # await client.wait_until_ready()
+            print("Offline")
+
+        await asyncio.sleep(60)
+
+client.run(discord_Token)
