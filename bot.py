8b108fe7ff9bb27c39f992daa0c521eeadca86e4
KimH4nKyul <alskim93@kakao.com>
2021-04-23 21:31:52 +0900

    change discrod bot state

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ twitch_Client_ID = os.environ['twitchcid']
 twitch_Client_secret = os.environ['twitchcsecret']
 discord_Token = os.environ['token']
 discord_channelID = os.environ['channel']
-discord_bot_state = ''
+discord_bot_state = '티배깅 존나'
 twitchID = 'tattoob0y'
 # twitchID = 'screamdaddy93'
 msg = ''
@@ -23,6 +23,7 @@ client = commands.Bot(command_prefix='!타봇 ', help_command=None)
 
 @client.command(name='안녕')
 async def hello(ctx):
+    ctx.message.author.id
 
     await ctx.send("안녕하세요? 타뱅온봇입니다. 현재는 개발중으로, 명령어가 없습니다. 기능 문의는 스크림아빠를 찾아주세요!")
 
